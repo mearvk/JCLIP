6b3af845b84ac9ac3adadf42b8634a69e724aae7
unknown <Max Rupplin@Desktop.(none)>
2011-11-19 17:29:13 -0500

    refined JCLIP's impl

--- src/org/jclip/JCLIP.java
+++ src/org/jclip/JCLIP.java
@@ -3,35 +3,52 @@ package org.jclip;
 import org.jclip.exceptions.NoMatchFoundException;
 import org.jclip.matcher.Matcher;
 import org.jclip.options.OptionGroup;
+import org.jclip.options.OptionGroups;
 import org.jclip.validation.Validator;
 
 public class JCLIP 
 {
-	private Matcher matcher=null;
-	private Validator validator=null;
+	public Matcher matcher=null;
+	public Validator validator=null;
+	public OptionGroup matchingGroup = null;
 	
-	public JCLIP(String...args)
+	public JCLIP(String...args) throws Exception
 	{
-		try
-		{
-			matcher = new Matcher(args);
-			validator = new Validator(args);
-		}
-		catch(Exception e)
-		{
-			e.printStackTrace();
-		}		
+		matcher = new Matcher(args);
+		validator = new Validator(args);	
 	}
 		
 	public void run() throws Exception
 	{		
-		OptionGroup matchingOptionGroup = matcher.match();
+		parseArgs();
 		
-		if(matchingOptionGroup==null) 
-			throw new NoMatchFoundException();
+		validateValues();
 		
-		validator.validate(matchingOptionGroup);
+		doCallbacks();
+	}
+	
+	public void setOptionGroups(OptionGroups groups)
+	{
+		this.matcher.optionGroups=groups;
+	}
+	
+	public void parseArgs() throws Exception
+	{
+		if(this.matcher==null) throw new NullPointerException("Matcher cannot be null!");
 		
-		matcher.doCallbacks();
+		this.matchingGroup = matcher.match();
+	}
+	
+	public void validateValues() throws Exception
+	{
+		if(this.validator==null) throw new NullPointerException("Validator cannot be null!");
+		if(this.matchingGroup==null) throw new NullPointerException("Must parse command line arguments before validating the associated values!");
+		
+		this.validator.validate(this.matchingGroup);
+	}
+	
+	public void doCallbacks()
+	{
+		this.matcher.doCallbacks();
 	}
 }
