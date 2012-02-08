511fc3439b2d55cf6d94af34eec03e7fc52382c7
unknown <Max Rupplin@HomeUser-PC.(none)>
2012-02-07 20:24:18 -0500

    Added more devious tests

--- src/org/jclip/testing/Test15.java
+++ src/org/jclip/testing/Test15.java
@@ -1,15 +1,77 @@
 package org.jclip.testing;
 
+import static org.junit.Assert.assertTrue;
+
+import org.jclip.JCLIP;
+import org.jclip.interfaces.Callback;
+import org.jclip.options.OptionGroup;
+import org.jclip.options.OptionGroups;
+import org.jclip.options.OptionalOption;
+import org.junit.Test;
+
 /**
- * Tests whether best but non-perfect OptionGroup is chosen when given an acceptable alternative
+ * Tests whether best but non-perfect OptionGroup is chosen when given a worse but acceptable alternative
  * 
  * @author Max Rupplin
  *
  */
 public class Test15 extends BaseTest 
 {
+	String[] args = new String[]{"--opt1=y", "--opt2=n"};
+	private static String expectedResult = "Test15.Callback2";
+	private static String actualResult = null;
+	
+	@Override
+	@Test
 	public void run()
 	{
-		
+		try
+		{
+			JCLIP jclp = new JCLIP(args);
+			
+			OptionGroup og1 = new OptionGroup("Test15.og1");
+			og1.addOptionalOption(new OptionalOption("opt1"));
+			og1.addOptionalOption(new OptionalOption("opt2"));
+			og1.addOptionalOption(new OptionalOption("opt3"));
+			og1.addOptionalOption(new OptionalOption("opt4"));
+			og1.addCallback(new Callback1());
+			
+			OptionGroups.addOptionGroup(og1);
+			
+			OptionGroup og2 = new OptionGroup("Test15.og2");
+			og2.addOptionalOption(new OptionalOption("opt1"));
+			og2.addOptionalOption(new OptionalOption("opt2"));
+			og2.addOptionalOption(new OptionalOption("opt3"));
+			
+			og2.addCallback(new Callback2());
+			
+			OptionGroups.addOptionGroup(og2);
+				
+			jclp.run();
+			
+			assertTrue(expectedResult.equals(actualResult));
+		}
+		catch (Exception e)
+		{						
+			e.printStackTrace(); 
+		}
 	}
+	
+	private class Callback1 implements Callback
+	{
+		@Override
+		public void execute()
+		{
+			Test15.actualResult = "Test14.Callback1";
+		}
+	}
+
+	private class Callback2 implements Callback
+	{
+		@Override
+		public void execute()
+		{
+			Test15.actualResult = "Test14.Callback2";
+		}
+	}	
 }
--- src/org/jclip/testing/Test16.java
+++ src/org/jclip/testing/Test16.java
@@ -1,5 +1,15 @@
 package org.jclip.testing;
 
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
+
+import org.jclip.JCLIP;
+import org.jclip.exceptions.AmbiguousMatchException;
+import org.jclip.options.OptionGroup;
+import org.jclip.options.OptionGroups;
+import org.jclip.options.OptionalOption;
+import org.junit.Test;
+
 /**
  * Tests whether two equally good but non-perfect matches kick up an AmbigousMatchException
  * 
@@ -8,8 +18,38 @@ package org.jclip.testing;
  */
 public class Test16 extends BaseTest 
 {
+	String[] args = new String[]{"--opt1=y", "--opt2=n"};
+	
+	@Override
+	@Test
 	public void run()
 	{
-		
+		try
+		{
+			JCLIP jclp = new JCLIP(args);
+			
+			OptionGroup og1 = new OptionGroup("Test15.og1");
+			og1.addOptionalOption(new OptionalOption("opt1"));
+			og1.addOptionalOption(new OptionalOption("opt2"));
+			og1.addOptionalOption(new OptionalOption("missing1"));
+			
+			OptionGroups.addOptionGroup(og1);
+			
+			OptionGroup og2 = new OptionGroup("Test15.og2");
+			og2.addOptionalOption(new OptionalOption("opt1"));
+			og2.addOptionalOption(new OptionalOption("opt2"));
+			og2.addOptionalOption(new OptionalOption("missing2"));
+
+			OptionGroups.addOptionGroup(og2);
+				
+			jclp.run();
+			
+			//shouldn't get here
+			fail();
+		}
+		catch (Exception e)
+		{						
+			assertTrue("Test16 caught a weird exception.", e instanceof AmbiguousMatchException);
+		}
 	}
 }
